ef002ba60e51716aa461c6256d4e10d7c89fb23a
Vlatko Juric <juricv78gmail.com>
2023-10-17 10:49:07 +0200

    react challenge

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>AAA</Button>
+      <Button>Push it</Button>
+      <Button>push it</Button>
+      <Button>real good</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
